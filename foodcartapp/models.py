598c09b96a6af68038a802d267ce2fba7e039e4a
Olga <o.panova27@gmail.com>
2023-02-12 02:28:42 +0300

    Add cost in list orders

--- foodcartapp/models.py
+++ foodcartapp/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from django.db.models import F, Sum
 from django.core.validators import MinValueValidator
 
 from phonenumber_field.modelfields import PhoneNumberField
+
+
+class OrderQuerySet(models.QuerySet):
+    def calculation_cost(self):
+        return self.annotate(cost=Sum(F('products__quantity') * F('products__product__price')))
 
 
 class Restaurant(models.Model):
@@ -141,6 +147,8 @@ class Order(models.Model):
     phonenumber = PhoneNumberField(
         'Мобильный номер'
     )
+
+    objects = OrderQuerySet.as_manager()
 
     class Meta:
         verbose_name = 'Заказ'
