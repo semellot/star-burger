dd17ab31a6c81d1d4a0ba9224420170263711187
Olga <o.panova27@gmail.com>
2023-03-09 21:11:58 +0300

    Use serializer creator

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -29,6 +29,22 @@ class OrderItemSerializer(serializers.ModelSerializer):
 class OrderSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(read_only=True)
     products = OrderItemSerializer(many=True, allow_empty=False, write_only=True)
+    address=serializers.CharField(max_length=100)
+    firstname=serializers.CharField(max_length=50)
+    lastname=serializers.CharField(max_length=50)
+    phonenumber=serializers.CharField(max_length=50)
+
+    def create(self, validated_data):
+        order = Order.objects.create(
+            address=validated_data['address'],
+            firstname=validated_data['firstname'],
+            lastname=validated_data['lastname'],
+            phonenumber=PhoneNumber.from_string(validated_data['phonenumber'])
+        )
+        products_fields = validated_data['products']
+        products = [OrderItem(order=order, price=fields['quantity']*fields['product'].price, **fields) for fields in products_fields]
+        OrderItem.objects.bulk_create(products)
+        return order
 
     class Meta:
         model = Order
@@ -93,16 +109,6 @@ def register_order(request):
     serializer.is_valid(raise_exception=True)
 
     with transaction.atomic():
-        order = Order.objects.create(
-            address=serializer.validated_data['address'],
-            firstname=serializer.validated_data['firstname'],
-            lastname=serializer.validated_data['lastname'],
-            phonenumber=PhoneNumber.from_string(serializer.validated_data['phonenumber'])
-        )
-        
-        products_fields = serializer.validated_data['products']
-        products = [OrderItem(order=order, price=fields['quantity']*fields['product'].price, **fields) for fields in products_fields]
-        OrderItem.objects.bulk_create(products)
-        
+        order = serializer.save()
         order_serializer = OrderSerializer(order)
         return Response(order_serializer.data)
